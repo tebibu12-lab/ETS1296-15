04457d976b311beee0d0819772a45b3830299e62
tebibu12-lab <stebibu42@gmail.com>
2025-05-12 14:58:06 +0300

    Create copy.py

---
+++ listmethods/copy.py
@@ -0,0 +1,5 @@
+fruits = ['apple', 'banana', 'cherry']
+print(f"The fruit bowl contains {', '.join(fruits)}!")
+print("The fruits decide to make copies of themselves for a grand feast!")
+new_fruits = fruits.copy()
+print(f"Now we have double the fun: {', '.join(new_fruits)}! 🎉🍎🍌🍒")
