69121268e9f8d35c3dd9f17ce61a974e06dff5e4
tebibu12-lab <stebibu42@gmail.com>
2025-05-12 14:57:27 +0300

    Create clear.py

---
+++ listmethods/clear.py
@@ -0,0 +1,5 @@
+fruits = ['apple', 'banana', 'cherry']
+print(f"The fruit bowl is full with {', '.join(fruits)}!")
+print("Oh no! The fruits have been taken away...")
+fruits.clear()
+print("The bowl is now empty. The party is over. 😢")
