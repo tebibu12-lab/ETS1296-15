bb22478048840a6e75dec6948750413854241945
tebibu12-lab <stebibu42@gmail.com>
2025-05-12 14:56:48 +0300

    Create append.py

---
+++ listmethods/append.py
@@ -0,0 +1,6 @@
+fruits = ['apple', 'banana', 'cherry']
+print(f"The party starts with {', '.join(fruits)}!")
+
+new_fruit = 'orange'
+fruits.append(new_fruit)
+print(f"{new_fruit} joins the party! Now we have {', '.join(fruits)}!")
